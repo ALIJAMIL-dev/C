a81b19af6c8de107be12fabffee7d1144a6e8588
ALI JAMIL <alijamil@outlook.com.tr>
2026-01-04 17:15:41 +0300

    C - Basics - 3 - Identifiers

--- 1. Basics/main.c
+++ 1. Basics/main.c
@@ -1,9 +1,16 @@
-// Basics - 2
+// Basics - 3
 #include <stdio.h>
+
+int sum(int a, int b)
+{
+    return a + b;
+}
 
 int main(void) 
 {
-    printf("Hi");
+    int var;
+    var = 9;
+    printf("%d\n", var);
+    printf("%d\n", sum(10,10));
     return 0;
 }
-   
